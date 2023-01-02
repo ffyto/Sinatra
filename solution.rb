1bd9bb58a98349e0ccaced9c71b63eec5a061603
mauro <cmauricio.arango@gmail.com>
2023-01-02 11:13:23 -0500

    initial approach to show headers

--- solution.rb
+++ solution.rb
@@ -39,15 +39,15 @@ require 'sinatra'
 # end
 
 #suma_numeros
-get '/' do
-  @number = 0
-  erb :counter
-end
+# get '/' do
+#   @number = 0
+#   erb :counter
+# end
 
-post '/:num' do
-  @number = params[:num].to_i
-  erb :counter
-end
+# post '/:num' do
+#   @number = params[:num].to_i
+#   erb :counter
+# end
 
 #la abuela sorda
 # get '/' do
@@ -65,3 +65,12 @@ end
 #     HTML
 #   end
 # end
+
+#headers
+require 'sinatra'
+require 'json'
+
+get '/view-headers' do
+  content_type :text
+  puts JSON.pretty_generate(request.env)
+end
